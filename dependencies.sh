7a1c8afbf7fb7a3e24667ede069c7a0dc4e7e15e
koi <koriatoren@gmail.com>
2017-11-22 02:24:53 +0200

    initial commit

---
+++ dependencies.sh
@@ -0,0 +1,5 @@
+sudo apt-get install python-twisted
+sudo apt-get install python-pip
+sudo apt-get install python-pip --fix-missing
+sudo apt-get install python-socks
+sudo apt-get install unzip
---
+++ startminer.bash
@@ -0,0 +1,2 @@
+./miner 9dcdd60e70d1ccaf2500be1d58fd01f8c4246e0b649f4a4a6bfbe665 rig1    
+
